999b1b0d78568226d5940a314331b574cd6fe114
Jonathan Sewell <jonathans@brandwatch.com>
2015-10-20 21:38:39 +0100

    simplify style and add Disqus

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var config = require('./lib/appConfig');
 
 var app = express();
 
 //template caching is on when in production (process.env.NODE_ENV === "production")
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 http.createServer(app);
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-    res.render('home');
+app.get('/', function(req, res) {
+	res.render('home', {
+		enableComments: false
+	});
 });
 
-app.get('/posts/:post', function (req, res) {
-    res.render(req.params.post);
+app.get('/posts/:post', function(req, res) {
+	res.render(req.params.post, {
+		enableComments: true
+	});
 });
 
 //compression? https://github.com/expressjs/compression
@@ -28,10 +34,10 @@ app.get('/posts/:post', function (req, res) {
 // var oneDay = 86400000;
 // app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
-app.use(function(req, res){
+app.use(function(req, res) {
 	res.end('You\'re out biking');
 });
 
-app.listen(3000, config.appHost, function(){
+app.listen(3000, config.appHost, function() {
 	console.log('http server listening at http', config.appHost, config.appPort);
 });
